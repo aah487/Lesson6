5c0ffbbc2f64f03b6e8e4b27da5245153f65e185
albert herrera <aah487@gmail.com>
2015-02-23 22:08:43 -0500

    update

--- networks.js
+++ networks.js
@@ -1,12 +1,17 @@
+//Cheet sheet and functions//
+//---hide--//
 
 $(function(){
 $(".amc,.hbo").hide() 
+
+//---click and fade---//
 
 $("#clickamc").on("click", function(){
 $(".amc").fadeIn()
 $("#clickhbo,#clickamc").fadeOut()
 })
 
+//---Click , fadeout and alert---//
 $("#yes").on("click", function(){
 $("#saul,#bad,#mad,#yes,#no,.q1").fadeOut(1000, function(){
 alert("The Walking Dead and Talking Dead are your best bet.");
@@ -25,12 +30,13 @@ $(".hbo").fadeIn()
 $("#clickhbo,#clickamc").fadeOut()
 });
 
-
+//---click , slideup and timing---//
 
 $("#yes2").on("click", function(){
 $("#games,#no2,#yes2,.q2").slideUp(1000)
 });
 
+//---click and slideup---///
 $("#no2").on("click", function(){
 $("#vice,#valley,#girls,#toget,#yes2,#no2,.q2").slideUp()
 
@@ -40,6 +46,8 @@ $("#vice,#valley,#girls,#toget,#yes2,#no2,.q2").slideUp()
 
 
 } );
+
+//---toggle---//
 
 //$(".beers #corona").on("click", function() {
  //$("#schools").fadeToggle(1000, function() {
